9259d045c682d3d2e0b3678dfca64eca8c00ec08
Mort <martenbrunoo@gmail.com>
2022-04-25 13:51:51 +0200

    mm

--- cheats.py
+++ cheats.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'GOFprototype.ui'
-#
 # Created by: PyQt5 UI code generator 5.15.2
 #
 # WARNING: Any manual changes made to this file will be lost when pyuic5 is
