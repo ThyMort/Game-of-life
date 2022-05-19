4610120d479f8e1c406f50af8b476b00414eb16f
Mort <martenbrunoo@gmail.com>
2022-05-20 00:28:07 +0200

    m

--- grid.py
+++ grid.py
@@ -2,24 +2,24 @@ import pygame
 import time
 import numpy as np
 
-Svart = [10, 10, 10]
-Vit = (255, 255, 255) 
-Grid = (20, 30, 250)
+Svart = (10, 10, 10)
+Grid = (40, 40, 40)
 Zombie = (170, 170, 170)
-Size = 10
+Vit = (255, 255, 255) 
 
-
+tid = 0.5
+size = 100
 
 def update(screen, cells, storlek, with_progress = False):
-    ny_cell = np.zeros((cells.shape[0], cells.shape[1]))
+    nya_celler = np.zeros((cells.shape[0], cells.shape[1]))
 
     for rad, col in np.ndindex(cells.shape):
-        alive = np.sum(cells[rad-1:rad +2]) - cells[rad, col]
+        alive = np.sum(cells[rad-1:rad +2, col-1:col+2]) - cells[rad, col]
         färg = Svart if cells[rad, col] == 0 else Vit
 
         if cells[rad, col] == 1:
             if 2 <= alive <= 3: 
-                ny_cell[rad, col] = 1
+                nya_celler[rad, col] = 1
                 if with_progress:
                     färg = Vit
                 
@@ -30,19 +30,22 @@ def update(screen, cells, storlek, with_progress = False):
 
         else:
             if alive == 3:
-                ny_cell[rad, col] = 1
+                nya_celler[rad, col] = 1
                 if with_progress:
                     färg = Vit 
 
-        
         pygame.draw.rect(screen, färg, (col * storlek, rad * storlek, storlek -1, storlek -1))
         
-    return ny_cell
+    return nya_celler
+
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((8 * size, 6* size))
+    
+    button_1 = pygame.Rect(50, 100, 200, 50)
+    pygame.draw.rect(screen, (255, 0, 0), button_1)
 
     cells = np.zeros((60, 80))
     screen.fill(Grid)
@@ -52,24 +55,47 @@ def main():
     pygame.display.update()
 
     running = False
-
+    
     while True:
-        for event in pygame.event.get():
+        for event in pygame.event.get(): #quit
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-            
-            elif event.type == pygame.KEYDOWN:
+
+            elif event.type == pygame.KEYDOWN: # start
                 if event.key == pygame.K_SPACE:
                     running = not running
                     update(screen, cells, 10)
                     pygame.display.update()
+
+            pos = pygame.mouse.get_pos()
             
-            if pygame.mouse.get_pressed()[0]:
-                pos = pygame.mouse.get_pos()
+            if pygame.mouse.get_pressed()[0]: #placera cell
                 cells[pos[1] // 10, pos[0] // 10] = 1
                 update(screen, cells, 10)
                 pygame.display.update()
+
+            button = pygame.Rect(50, 100, 200, 50)
+            
+            if button.collidepoint(pos):
+                if click:
+                    options()
+
+ 
+            click = False
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                if event.type == MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        click = True
+ 
+            pygame.display.update()
 
         screen.fill(Grid)
 
@@ -77,7 +103,23 @@ def main():
             cells = update(screen, cells, 10, with_progress = True)
             pygame.display.update()
 
-        time.sleep(0.001)
+        time.sleep(tid)
+
+def options():
+    running_options = True
+    
+    while running_options:
+        screen.fill((Vit))
+        'options', (255, 255, 255), screen, 20, 20
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+        
+        pygame.display.update()
 
 
 if __name__ == '__main__':
